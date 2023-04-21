fc42c5a850cab1b0cb0342409f9b7c385e9bf740
Pito Salas <pitosalas@users.noreply.github.com>
2023-04-21 14:09:31 -0400

    try this

--- maksummary.rb
+++ maksummary.rb
@@ -1,18 +1,25 @@
-def list_directories(path, level = 0)
-  indentation = ' ' * (level * 4) + '*'
+# Write a ruby script which generates text output as follows:
+# Go through the current directory, recursively
+# Directories are shown with their names in square brackets, followed directly by a parenthesis, the current path followed by the "README.md" and a closed parenthesis
+# Files with a `.md` extension are shown with their names in square brackets and their full paths in parentheses.
+# Directories named "images" or “docs” are skipped.
+# Files named SUMMARY.md and READNE.md are skipped
+# Each line is displayed with an indentation followed by a star and a space. The indentation starts at zero and goes up by four each level of the directory
+
+require 'find'
+
+def process_directory(path, depth = 0)
+  indent = ' ' * depth * 4
   Dir.glob("#{path}/*").sort.each do |entry|
+    next if entry.end_with?('README.md') || entry.end_with?('SUMMARY.md')
     if File.directory?(entry)
-      dir_name = File.basename(entry)
-      next if dir_name == "images" || dir_name = "docs"
-      readme_path = "#{entry}/README.md"
-      puts "#{indentation} [#{dir_name}](#{readme_path})"
-      list_directories(entry, level + 1)
-    elsif File.extname(entry) == ".md"
-      md_file = File.basename(entry)
-      puts "#{indentation} [#{md_file}](#{entry})"
+      next if entry.end_with?('images') || entry.end_with?('docs')
+      puts "#{indent}* [#{File.basename(entry)}](#{entry}/README.md)"
+      process_directory(entry, depth + 1)
+    elsif entry.end_with?('.md')
+      puts "#{indent}* [#{File.basename(entry, '.md')}](#{entry})"
     end
   end
 end
 
-root_directory = '.' # Change this to the desired directory path
-list_directories(root_directory)
+process_directory('.')
