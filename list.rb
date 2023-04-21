2675d174ca11e6c26b13897d59fc68da504b2bc6
Pito Salas <pitosalas@users.noreply.github.com>
2023-04-21 11:49:38 -0400

    try this

---
+++ list.rb
@@ -0,0 +1,14 @@
+def list_directories(path, level = 0)
+  indentation = ' ' * level + '*'
+  Dir.glob("#{path}/*").sort.each do |entry|
+    if File.directory?(entry)
+      dir_name = File.basename(entry)
+      readme_path = "#{entry}/README.md"
+      puts "#{indentation} [#{dir_name}] (#{readme_path})"
+      list_directories(entry, level + 1)
+    end
+  end
+end
+
+root_directory = '.' # Change this to the desired directory path
+list_directories(root_directory)
