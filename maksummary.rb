70df613fdf5ef36ce979c8ae0676577f83201132
Pito Salas <pitosalas@users.noreply.github.com>
2023-04-27 14:11:04 -0400

    new

--- maksummary.rb
+++ maksummary.rb
@@ -11,9 +11,9 @@ class MakeSummary
 
   def generate_entry_for_file(entry, path, indent)
     file_keys = extract_keys_from_markdown(entry, path)
-    title = file_keys.fetch("title", entry)
-    order = file_keys.fetch("order", 100)
-    text_string = " #{'    ' * indent}* [#{title}](#{path})"
+    title = file_keys.fetch(:title, entry)
+    order = file_keys.fetch(:order, 100)
+    text_string = "#{'    ' * indent}* [#{title}](#{path})"
     {text: text_string, indent:, title:, order:}
   end
 
@@ -34,7 +34,8 @@ class MakeSummary
     info_file = File.join(current_path, "info.yml")
     if File.exist?(info_file)
       info = YAML.load_file(info_file)
-      return info["title"] if info["title"]
+      info.transform_keys!(&:to_sym)
+      return info[:title] if info[:title]
     end
     entry
   end
@@ -44,8 +45,10 @@ class MakeSummary
     info_file = File.join(current_path, "info.yml")
     if File.exist?(info_file)
       info = YAML.load_file(info_file)
-      title = info.fetch("title", entry)
-      order = info.fetch("order", 100)
+      info.transform_keys!(&:to_sym)
+
+      title = info.fetch(:title, entry)
+      order = info.fetch(:order, 100)
       result.merge!(order:, title:)
     end
     result
@@ -55,6 +58,7 @@ class MakeSummary
     result = {title: entry}
     begin
       md_content = YAML.load_file(current_path)
+      md_content.transform_keys!(&:to_sym)
     rescue StandardError
     end
     result.merge!(md_content) unless md_content.nil? || !md_content.is_a?(Hash)
@@ -63,13 +67,12 @@ class MakeSummary
 
   def process(current, depth)
     all_entries = []
-    puts "processing #{current}"
     Dir.entries(current).each do |entry|
       next if skip_entry?(entry)
-      current_path = File.join(".", entry)
+      current_path = File.join(current, entry)
       if File.directory?(current_path)
         dir_info = extract_dir_info(entry, current_path, depth)
-        dir_entries = process_directory(current_path, depth + 1)
+        dir_entries = process(current_path, depth + 1)
         x = dir_info.merge!(lines: dir_entries)
         all_entries << x
       elsif valid_markdown_file?(entry)
@@ -81,7 +84,8 @@ class MakeSummary
   end
 
   def generate_output(all_entries)
-    all_entries.each do |entry|
+    all_entries.sort_by { |x| x[:order] }
+               .each do |entry|
       puts entry[:text]
       next unless entry[:lines]
       entry[:lines].sort_by { |x| x[:order] }.each do |line|
@@ -94,24 +98,6 @@ class MakeSummary
     current_path = File.join(".")
     all_entries  = process(current_path, 0)
     generate_output(all_entries)
-  end
-
-## DELETABLE
-
-  def process_directory(path, indent = 0)
-    entries = []
-    Dir.entries(path).sort.each do |entry|
-      next if skip_entry?(entry)
-      current_path = File.join(path, entry)
-      if File.directory?(current_path)
-        dir_info = extract_dir_info(entry, current_path, indent)
-        lines = process_directory(current_path, indent + 1)
-        entries << dir_info.merge!(lines:)
-      elsif valid_markdown_file?(entry)
-        entries << generate_entry_for_file(entry, current_path, indent)
-      end
-    end
-    entries
   end
 end
 
